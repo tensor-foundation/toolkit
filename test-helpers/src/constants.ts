6ec674c5622fcf87062cf9dcf1cb164118ab622b
febo <febo@tensor.so>
2024-05-17 21:46:04 -0400

    Update test-helpers

--- test-helpers/src/constants.ts
+++ test-helpers/src/constants.ts
@@ -1,4 +1,19 @@
+/**
+ * Lamports in one Sol.
+ */
 export const LAMPORTS_PER_SOL = 1_000_000_000n;
 
+/**
+ * Rent-exempt reserve for new accounts.
+ */
 export const ZERO_ACCOUNT_RENT_LAMPORTS = 890880n;
-export const ONE_SOL = 1_000_000_000n;
+
+/**
+ * Lamports in one Sol.
+ */
+export const ONE_SOL = 1n * LAMPORTS_PER_SOL;
+
+/**
+ * Creates a lamports value from a sol value.
+ */
+export const sol = (amount: number) => amount * Number(LAMPORTS_PER_SOL);
