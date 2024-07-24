a878c20bc3f3167678c5c5581a68aaa75d844d8e
leantOnSol <leantOnSol@proton.me>
2024-07-24 20:00:46 +0200

    fix exports

--- test-helpers/src/index.ts
+++ test-helpers/src/index.ts
@@ -1,6 +1,5 @@
 export * from './addresses';
 export * from './constants';
-export * from '../../mpl-bubblegum/src/compression';
 export * from './programIds';
 export * from './setup';
 export * from './token';
