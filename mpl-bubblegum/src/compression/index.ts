28981a10cfaf34a5e601973d2e349a0401355460
leantOnSol <leantOnSol@proton.me>
2024-07-25 01:05:51 +0200

    setup function

--- mpl-bubblegum/src/compression/index.ts
+++ mpl-bubblegum/src/compression/index.ts
@@ -1,6 +1,7 @@
 export * from './hashing'
 export * from './helpers'
 export * from './merkleTree';
+export * from './setup';
 export * from './token';
 export * from './tree';
 export * from './verification';
