61bdcee4a5be9b8978fa8dfc19c355e2223c8976
Samuel Vanderwaal <samv@tensor.so>
2024-08-13 08:36:01 -0800

    regen mpl-token-metadata; remove linting for this package

--- mpl-token-metadata/src/generated/instructions/createMetadataAccountV3.ts
+++ mpl-token-metadata/src/generated/instructions/createMetadataAccountV3.ts
@@ -239,7 +239,7 @@ export function getCreateMetadataAccountV3Instruction<
       getAccountMeta(accounts.updateAuthority),
       getAccountMeta(accounts.systemProgram),
       getAccountMeta(accounts.rent),
-    ].filter(<T>(x: T | undefined): x is T => x !== undefined),
+    ].filter(<T,>(x: T | undefined): x is T => x !== undefined),
     programAddress,
     data: getCreateMetadataAccountV3InstructionDataEncoder().encode(
       args as CreateMetadataAccountV3InstructionDataArgs
