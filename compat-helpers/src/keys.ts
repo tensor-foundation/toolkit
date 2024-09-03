a2077db44a40fd81d0a3637e1162c7956ea0066c
Samuel Vanderwaal <samv@tensor.so>
2024-09-03 12:59:49 -0800

    fix name for consistency

--- compat-helpers/src/keys.ts
+++ compat-helpers/src/keys.ts
@@ -2,7 +2,7 @@ import { fromLegacyKeypair } from '@solana/compat';
 import { Keypair } from '@solana/web3.js';
 import { createSignerFromKeyPair, KeyPairSigner } from '@solana/web3.js-next';
 
-export async function fromLegacyKeypairIntoKeyPairSigner(
+export async function fromLegacyKeypairToKeyPairSigner(
   keypair: Keypair
 ): Promise<KeyPairSigner> {
   return await createSignerFromKeyPair(await fromLegacyKeypair(keypair));
