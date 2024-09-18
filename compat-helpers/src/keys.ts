6e22b32832707127c0ab6c965e026387a9a0a8ab
leant <155785958+leantOnSol@users.noreply.github.com>
2024-09-18 21:25:27 +0200

    new compat helper for pubkey => noopSigner (#57)
    * new compat helper for pubkey => noopSigner
    
    * whoops, brainfart on my end

--- compat-helpers/src/keys.ts
+++ compat-helpers/src/keys.ts
@@ -1,9 +1,13 @@
 import { fromLegacyKeypair } from '@solana/compat';
-import { Keypair } from '@solana/web3.js';
-import { createSignerFromKeyPair, KeyPairSigner } from '@solana/web3.js-next';
+import { Keypair, PublicKey } from '@solana/web3.js';
+import { createSignerFromKeyPair, KeyPairSigner, NoopSigner, address, createNoopSigner } from '@solana/web3.js-next';
 
 export async function fromLegacyKeypairToKeyPairSigner(
   keypair: Keypair
 ): Promise<KeyPairSigner> {
   return await createSignerFromKeyPair(await fromLegacyKeypair(keypair));
+}
+
+export function fromPublicKeyToNoopSigner(publickey: PublicKey): NoopSigner {
+  return createNoopSigner(address(publickey.toBase58()));
 }
