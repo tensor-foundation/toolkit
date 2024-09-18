544d504b2bccbbd37ef60e1a6c5f505c351abdcf
leantOnSol <leantOnSol@proton.me>
2024-09-18 20:21:53 +0200

    new compat helper for pubkey => noopSigner

--- compat-helpers/src/keys.ts
+++ compat-helpers/src/keys.ts
@@ -1,9 +1,18 @@
 import { fromLegacyKeypair } from '@solana/compat';
-import { Keypair } from '@solana/web3.js';
-import { createSignerFromKeyPair, KeyPairSigner } from '@solana/web3.js-next';
+import { Keypair, PublicKey } from '@solana/web3.js';
+import { createSignerFromKeyPair, KeyPairSigner, NoopSigner, address } from '@solana/web3.js-next';
 
 export async function fromLegacyKeypairToKeyPairSigner(
   keypair: Keypair
 ): Promise<KeyPairSigner> {
   return await createSignerFromKeyPair(await fromLegacyKeypair(keypair));
+}
+
+export function fromPublicKeyToNoopSigner(publickey: PublicKey): NoopSigner {
+  const out: NoopSigner = {
+      address: address(publickey.toBase58()),
+      signMessages: messages => Promise.resolve(messages.map(() => Object.freeze({}))),
+      signTransactions: transactions => Promise.resolve(transactions.map(() => Object.freeze({}))),
+  };
+  return Object.freeze(out);
 }
