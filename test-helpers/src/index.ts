d6e87ee388202adc6a48e7224bd86282c1232182
Samuel Vanderwaal <samv@tensor.so>
2024-10-12 09:40:43 -0800

    add anchor errors (#62)

--- test-helpers/src/index.ts
+++ test-helpers/src/index.ts
@@ -2,5 +2,6 @@ export * from './addresses';
 export * from './constants';
 export * from './programIds';
 export * from './setup';
+export * from './shared';
 export * from './token';
 export * from './wns';
